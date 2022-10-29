c23f2ae4b703b3dc2128811ed4354a96dde4ddd7
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2022-10-29 14:37:24 -0700

    fix error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,13 +13,14 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		if (str[i] == 32 || str[i] == 9 || str[i] == 44 || str[i] == 59
-				|| str[i] == 46 || str[i] == 40 || str[i] == 41
-				|| str[i] == 63 || str[i] == 33 || str[i] == 34
-				|| str[i] == 123 || str[i] == 125)
+			|| str[i] == 46 || str[i] == 40 || str[i] == 41
+			|| str[i] == 63 || str[i] == 33 || str[i] == 34
+			|| str[i] == 123 || str[i] == 10 || str[i] == 12)
 		{
 			i++;
 			if (str[i] >= 97 && str[i] <= 122)
 				*(str + i) =  (str[i] - 32);
+			i--;
 		}
 		i++;
 	}
