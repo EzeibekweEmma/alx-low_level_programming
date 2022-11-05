dab09bab91ec136c022e51bd2e60a02786a4f290
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2022-11-04 20:09:13 -0700

    program that prints the minimum number of coins to make change for an amount of money

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: Argument counter
+ * @argv: Argument vector
+ * Return:  0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (argc == 1)
+			printf("0\n");
+	}
+
+
+	return (0);
+}
