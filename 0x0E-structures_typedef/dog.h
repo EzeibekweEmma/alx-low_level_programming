ee85738743bb8f1a51768899ea774c483282521d
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2022-11-14 20:39:01 +0100

    a new type struct dog with it  elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - About the dog.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ */
+
+struct dog 
+{
+	char * name;
+	float age;
+	char * owner;
+} dog;
+
+#endif
