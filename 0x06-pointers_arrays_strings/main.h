c8855f6cde55c2ad7dd86f604f5cc344af9cf4de
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2022-12-19 02:47:28 +0100

    added a prototype and fix an error

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/**
- *prototypes()
- */
+
+int _putchar(char c);
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
