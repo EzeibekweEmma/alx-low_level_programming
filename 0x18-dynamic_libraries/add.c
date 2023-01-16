af43da6670da1e4675686cca3fe556cfe0e9ce62
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-16 15:47:42 +0100

    creating operations functions

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,16 @@
+#include "operations.h"
+
+/**
+ * add - function that add two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Answer
+ */
+
+int add(int a, int b)
+{
+	int ans;
+
+	ans = a + b;
+	return (ans);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,16 @@
+#include "operations.h"
+
+/**
+ * sub - function that subtract two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Answer
+ */
+
+int sub(int a, int b)
+{
+	int ans;
+
+	ans = a - b;
+	return (ans);
+}
